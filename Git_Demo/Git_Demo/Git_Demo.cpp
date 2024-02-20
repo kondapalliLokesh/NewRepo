898ead9b156ce01563d0593c8070855288d44a45
kondapalliLokesh <kondapallilokesh999@gmail.com>
2024-02-20 07:58:16 +0530

    Add project files.

---
+++ Git_Demo/Git_Demo/Git_Demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	cout << "using v2022" << endl;
+	cout << "Dlg box Added" << endl;
+	cout << "Implemented file handing manchine" << endl;
+
+	return 0;
+}
